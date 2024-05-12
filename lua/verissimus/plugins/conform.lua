f1f60690b0c50adc48aca88284a8c8a4df71ef46
Tejas <tejassharma2021@outlook.com>
2024-05-12 17:42:19 +0530

    add-lsp-support-php

--- lua/verissimus/plugins/conform.lua
+++ lua/verissimus/plugins/conform.lua
@@ -17,15 +17,26 @@ return {
 				markdown = { "prettier" },
 				graphql = { "prettier" },
 				liquid = { "prettier" },
+				php = { "php-cs-fixer" },
 				lua = { "stylua" },
 				python = { "isort", "black" },
 			},
 			formatters = {
+				["php-cs-fixer"] = {
+					command = "php-cs-fixer",
+					args = {
+						"fix",
+						"--rules=@PSR12", -- Formatting preset. Other presets are available, see the php-cs-fixer docs.
+						"$FILENAME",
+					},
+					stdin = false,
+				},
 				prettier = {
 					command = "/home/verissimus/.local/share/nvim/mason/bin/prettier",
 					-- Global options for prettier --
-					prepend_args = { "--use-tabs", "--single-quote", "--jsx-single-quote", "--bracket-same-line"},
+					prepend_args = { "--use-tabs", "--single-quote", "--jsx-single-quote", "--bracket-same-line" },
 				},
+				notify_on_error = true,
 			},
 		})
 
